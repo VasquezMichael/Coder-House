a8400539b76e28b821de3aa10c0e8361cfb7a3a0
VasquezMichael <vasquez.michael.steven@gmail.com>
2022-06-22 13:34:11 -0300

    Cambiamos la estructura html y sumamos Bootstrap, ademas avanzamos con el Js, obtenemos en forma dinamica que producto añadimos al carrito

--- JavaScript/carrito-de-compras/script/script.js
+++ JavaScript/carrito-de-compras/script/script.js
@@ -1,14 +1,66 @@
-let total = 0;
-let butomsAdd = Array.from(document.getElementsByClassName('button-add'));
-let buttomPay = document.getElementById('pay');
+const butomsAdd = document.querySelectorAll('.button');
+const tbody = document.querySelector('.tbody');
+let carrito = [];
 
 butomsAdd.forEach((element) => {
-  element.addEventListener('click', () => {
-    total += 500;
-    buttomPay.innerHTML = `Pagar $${total}`;
-  });
+  element.addEventListener('click', addToCarritoItem);
 });
 
-buttomPay.addEventListener('click', () => {
+function addToCarritoItem(e) {
+  const button = e.target;
+  const item = button.closest('.card');
+  const itemTitle = item.querySelector('.card-title').textContent;
+  const itemPrice = item.querySelector('.precio').textContent;
+  const itemImg = item.querySelector('.card-img-top').src;
+  const newItem = {
+    title: itemTitle,
+    price: itemPrice,
+    img: itemImg,
+    cant: 1,
+  };
+
+  addItemCarrito(newItem);
+}
+
+function addItemCarrito(newItem) {
+  for (let index = 0; index < carrito.length; index++) {
+    if (carrito[index].title.trim() === newItem.title.trim()) {
+      carrito[index].cant++;
+      console.log(carrito);
+      return null;
+    }
+  }
+
+  carrito.push(newItem);
+  renderCarrito();
+}
+
+function renderCarrito() {
+  tbody.innerHTML = '';
+  carrito.map((item) => {
+    const tr = document.createElement('tr');
+    tr.classList.add('ItemCarrito');
+    const content = `
+
+      <th scope="row">1</th>
+      <td class="table__productos">
+          <img src=${item.img} alt="">
+          <h6 class="title">${item.title}</h6>
+      </td>
+      <td class="table__precio">
+          <p>${item.price}</p>
+      </td>
+      <td class="table__cantidad">
+          <input type="number" min="1" value=${item.cant}>
+          <button class="delete btn btn-danger">x</button>
+      </td>
+    
+    `;
+    tr.innerHTML = content;
+    tbody.appendChild(tr);
+  });
+}
+
+/*buttomPay.addEventListener('click', () => {
   alert('Total a pagar: ' + total);
-});
+});*/
